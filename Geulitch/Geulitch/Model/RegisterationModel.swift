7c952337d4b3810b0dab7f5f50643605e01d91e7
grgnjhyxxk <grgnjhyxxk@gmail.com>
2024-10-01 20:35:07 +0900

    기본 회원가입 시 필요한 데이터 model로 정리(ViewModel과의 연결을 위해)

---
+++ Geulitch/Geulitch/Model/RegisterationModel.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterationModel.swift
+//  Geulitch
+//
+//  Created by Jaehyeok Lim on 9/28/24.
+//
+
+struct RegistrationModel {
+    var phoneNumber: String?
+    var userID: String?
+    var password: String?
+    var penName: String?
+}
+
