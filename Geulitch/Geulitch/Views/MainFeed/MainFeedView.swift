6eada61f907908f3288cbcb9daab70ede88a31e2
grgnjhyxxk <grgnjhyxxk@gmail.com>
2024-10-11 16:47:28 +0900

    기본 세팅

---
+++ Geulitch/Geulitch/Views/MainFeed/MainFeedView.swift
@@ -0,0 +1,30 @@
+//
+//  MainFeedView.swift
+//  Geulitch
+//
+//  Created by Jaehyeok Lim on 10/11/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MainFeedView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+
+    }
+    
+    private func setupConstraints() {
+
+    }
+}
---
+++ Geulitch/Geulitch/Views/MainFeed/MainFeedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainFeedViewController.swift
+//  Geulitch
+//
+//  Created by Jaehyeok Lim on 10/11/24.
+//
+
+import UIKit
+
+class MainFeedViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
