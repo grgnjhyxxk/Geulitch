4538fc4b9294655ee4f6d5ab60c9eab8b05b4d5a
grgnjhyxxk <grgnjhyxxk@gmail.com>
2024-10-23 04:42:35 +0900

    init setting

---
+++ Geulitch/Geulitch/Views/Profile/Edit/ProfileInfoEditViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileInfoEditViewController.swift
+//  Geulitch
+//
+//  Created by Jaehyeok Lim on 10/22/24.
+//
+
+import UIKit
+
+class ProfileInfoEditViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
