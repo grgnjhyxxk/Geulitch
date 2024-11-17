f118ed8e8dd898dfeea4d8c6d84994237bf4c4b1
grgnjhyxxk <grgnjhyxxk@gmail.com>
2024-11-17 22:07:46 +0900

    TableViewDelegate 수정

--- Geulitch/Geulitch/Views/Profile/ContentsViewController.swift
+++ Geulitch/Geulitch/Views/Profile/ContentsViewController.swift
@@ -47,7 +47,6 @@ final class ContentsViewController: UIViewController, AquamanChildViewController
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.isLoading = false
             self.viewHolder.layoutView.reloadData()
-            self.viewHolder.layoutView.reloadData()
         }
     }
     
@@ -75,7 +74,7 @@ private extension ContentsViewController {
 extension ContentsViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if isLoading {
-            return 3
+            return 5
         } else {
             return contentType.cellCount
         }
@@ -83,8 +82,10 @@ extension ContentsViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if isLoading {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: SkeletonTableViewCell.identifier, for: indexPath) as? SkeletonTableViewCell else {
-             return UITableViewCell()
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SkeletonTableViewCell.identifier, for: indexPath) as? SkeletonTableViewCell else { return UITableViewCell() }
+            
+            cell.circleView.snp.updateConstraints { make in
+                make.top.equalTo(15)
             }
             
             DispatchQueue.main.async {
@@ -98,38 +99,36 @@ extension ContentsViewController: UITableViewDelegate, UITableViewDataSource {
             return cell
          }
         
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: RecommendedTableCustomCell.identifier, for: indexPath) as? RecommendedTableCustomCell else {
-            return UITableViewCell()
-        }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedTableCustomCell.identifier, for: indexPath) as? FeedTableCustomCell else { return UITableViewCell() }
         
         switch contentType {
         case .post:
-            cell.postTitleTextLabel.text = "포스트 \(indexPath.row + 1) 제목"
             cell.postBlurbTextLabel.text = "포스트 \(indexPath.row + 1)의 간단한 설명입니다. 이 설명은 포스트의 내용을 미리 보여줍니다. 길이를 더욱 늘리기 위한 발악."
         case .series:
-            cell.postTitleTextLabel.text = "시리즈 \(indexPath.row + 1) 제목"
             cell.postBlurbTextLabel.text = "시리즈 \(indexPath.row + 1)의 간단한 설명입니다. 이 설명은 시리즈의 내용을 미리 보여줍니다. 길이를 더욱 늘리기 위한 발악."
         }
-
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 188
+        return isLoading ? 120 : UITableView.automaticDimension
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        print("tab cell \(indexPath.row)")
+        print("\(indexPath.row)번 셀 클릭")
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         if isLoading {
             cell.separatorInset = UIEdgeInsets(top: 0, left: .greatestFiniteMagnitude, bottom: 0, right: 0)
+            tableView.allowsSelection = false
         } else {
             cell.separatorInset = .zero
+            tableView.allowsSelection = true
         }
+        
+        cell.selectionStyle = .none
     }
 }
 
@@ -142,7 +141,7 @@ extension ContentsViewController {
             view.showsVerticalScrollIndicator = false
             view.backgroundColor = UIColor.primaryBackgroundColor
             view.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-            view.register(RecommendedTableCustomCell.self, forCellReuseIdentifier: RecommendedTableCustomCell.identifier)
+            view.register(FeedTableCustomCell.self, forCellReuseIdentifier: FeedTableCustomCell.identifier)
             view.register(SkeletonTableViewCell.self, forCellReuseIdentifier: SkeletonTableViewCell.identifier)
             
             return view
