bd34667efeae86156ba84d548c3db0d191e27794
grgnjhyxxk <grgnjhyxxk@gmail.com>
2024-11-17 22:22:23 +0900

    각종 기본 ViewController의 변경 또는 추가로 인해 코드 수정

--- Geulitch/Geulitch/Views/TabBar/TabBarController.swift
+++ Geulitch/Geulitch/Views/TabBar/TabBarController.swift
@@ -7,98 +7,104 @@
 
 import UIKit
 
-class TabBarController: UITabBarController {
+final class TabBarController: UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabBar()
+        delegate = self // UITabBarControllerDelegate 설정
     }
 
     private func setupTabBar() {
         view.backgroundColor = UIColor.primaryBackgroundColor
-        
-        // 스크롤 안했을 때 배경색
         tabBar.barTintColor = UIColor.primaryBackgroundColor
-        
-        // 기본 배경색
         tabBar.backgroundColor = UIColor.primaryBackgroundColor
-        
-        // 아이콘 선택 시 색상
         tabBar.tintColor = UIColor.AccentButtonBackgroundColor
-        
-        // 아이콘 미선택 시 색상
-        tabBar.unselectedItemTintColor = UIColor.gray
-        
-        // 투명도
+        tabBar.unselectedItemTintColor = UIColor.systemGray
         tabBar.isTranslucent = false
-        
         tabBar.clipsToBounds = true
-        
         tabBar.shadowImage = UIImage()
-        
         tabBar.scrollEdgeAppearance?.backgroundColor = UIColor.primaryBackgroundColor
-        tabBar.scrollEdgeAppearance?.backgroundColor = UIColor.primaryBackgroundColor
-        
-        let mainFeedViewController = UINavigationController(rootViewController: MainFeedViewController())
-        let mainFeedViewIcon = UIImage(systemName: "house")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
-        let mainFeedViewSelectedIcon = UIImage(systemName: "house.fill")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
-        mainFeedViewController.tabBarItem = UITabBarItem(title: "",
-                                                         image: mainFeedViewIcon,
-                                                         selectedImage: mainFeedViewSelectedIcon)
 
-        let searchingViewController = UIViewController()
-        let searchingViewIcon = UIImage(systemName: "magnifyingglass")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
-        let searchingViewSelectedIcon = UIImage(systemName: "magnifyingglass")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
-        searchingViewController.tabBarItem = UITabBarItem(title: "",
-                                                          image: searchingViewIcon,
-                                                          selectedImage: searchingViewSelectedIcon)
-        
+        let mainFeedViewController = UINavigationController(rootViewController: MainFeedViewController())
+        mainFeedViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(systemName: "house")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold)),
+            selectedImage: UIImage(systemName: "house.fill")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
+        )
+
+        let searchingViewController = UINavigationController(rootViewController: RecommendedUserViewController())
+        searchingViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(systemName: "magnifyingglass")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold)),
+            selectedImage: UIImage(systemName: "magnifyingglass")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
+        )
+
         let imsiViewController = UIViewController()
-        let imsiViewIcon = UIImage(systemName: "bell")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
-        let imsiViewSelectedIcon = UIImage(systemName: "bell.fill")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
-        imsiViewController.tabBarItem = UITabBarItem(title: "",
-                                                     image: imsiViewIcon,
-                                                     selectedImage: imsiViewSelectedIcon)
-        
-        let activityViewController = UIViewController()
-        let activityViewIcon = UIImage(systemName: "heart")
-        let activityViewSelectedIcon = UIImage(systemName: "heart.fill")
-        activityViewController.tabBarItem = UITabBarItem(title: "",
-                                                        image: activityViewIcon,
-                                                        selectedImage: activityViewSelectedIcon)
-        
-        let myProfileViewController = UINavigationController(rootViewController: ProfileViewController())
-        let myProfileViewIcon = UIImage(systemName: "person")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
-        let myProfileViewSelectedIcon = UIImage(systemName: "person.fill")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
-        myProfileViewController.tabBarItem = UITabBarItem(title: "",
-                                                          image: myProfileViewIcon,
-                                                          selectedImage: myProfileViewSelectedIcon)
-        
+        imsiViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(systemName: "bell")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold)),
+            selectedImage: UIImage(systemName: "bell.fill")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
+        )
+
+        let activityViewController = MainFeedViewController()
+        activityViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(systemName: "heart"),
+            selectedImage: UIImage(systemName: "heart.fill")
+        )
+
+        let myProfileViewController = UINavigationController(rootViewController: LoggedInUserViewController(useCustomNavigation: true))
+        myProfileViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(systemName: "person")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold)),
+            selectedImage: UIImage(systemName: "person.fill")?.withConfiguration(UIImage.SymbolConfiguration(weight: .semibold))
+        )
+
+        viewControllers = [
+            mainFeedViewController,
+            searchingViewController,
+            imsiViewController,
+            activityViewController,
+            myProfileViewController
+        ]
+
+        addCenterButton() // 가운데 버튼 추가
+    }
+
+    private func addCenterButton() {
         let button = UIButton()
         button.setImage(UIImage(systemName: "plus")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 20, weight: .semibold)), for: .normal)
-        
         button.tintColor = UIColor.AccentButtonTextColor
         button.backgroundColor = UIColor.AccentButtonBackgroundColor
         button.layer.cornerRadius = 10
         view.addSubview(button)
-        
+
         button.snp.makeConstraints { make in
             make.bottom.equalToSuperview().offset(-45)
             make.centerX.equalToSuperview()
             make.size.equalTo(CGSize(width: 50, height: 35))
         }
-        
+
         button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        
-        viewControllers = [
-                                mainFeedViewController,
-                                searchingViewController,
-                                imsiViewController,
-                                activityViewController,
-                                myProfileViewController
-                          ]
     }
-    
+
     @objc func buttonAction() {
-        present(UIViewController(), animated: true)
+        let BasePostEditorVC = UINavigationController(rootViewController: BasePostEditorViewController())
+        BasePostEditorVC.modalPresentationStyle = .fullScreen
+        present(BasePostEditorVC, animated: true)
+    }
+
+    // UITabBarControllerDelegate 메서드 구현
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        guard let viewControllers = viewControllers else { return false }
+
+        // 클릭된 뷰 컨트롤러의 인덱스가 가운데(예: 세 번째) 뷰 컨트롤러인지 확인
+        let middleIndex = viewControllers.count / 2
+        if let index = viewControllers.firstIndex(of: viewController), index == middleIndex {
+            // 가운데 탭이 선택될 때 버튼 동작으로 분기
+            buttonAction()
+            return false // 탭 선택 방지
+        }
+        return true
     }
 }
