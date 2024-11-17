c6f0f3fb52c48f0e71d12e83d3edd1dc7279b8b9
grgnjhyxxk <grgnjhyxxk@gmail.com>
2024-11-17 20:30:58 +0900

    imageFromProfileLink 함수 제거

--- Geulitch/Geulitch/Manager/UtilitiesManager.swift
+++ Geulitch/Geulitch/Manager/UtilitiesManager.swift
@@ -22,31 +22,4 @@ class Utilities {
         let hashed = SHA256.hash(data: inputData)
         return hashed.compactMap { String(format: "%02x", $0) }.joined()
     }
-    
-    static func imageFromProfileLink(profileImageLink: String?, completion: @escaping (UIImage?) -> Void) {
-        // 프로필 이미지 링크가 비어있으면 기본 이미지 반환
-        guard let link = profileImageLink, !link.isEmpty, let url = URL(string: link) else {
-            completion(UIImage(named: "DefaultUserProfileImage"))
-            return
-        }
-        
-        // 이미지 다운로드
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("이미지 다운로드 오류: \(error)")
-                completion(UIImage(named: "DefaultUserProfileImage")) // 오류 발생 시 기본 이미지 반환
-                return
-            }
-            
-            guard let data = data, let image = UIImage(data: data) else {
-                completion(UIImage(named: "DefaultUserProfileImage")) // 이미지 변환 실패 시 기본 이미지 반환
-                return
-            }
-            
-            // 다운로드 성공 시 이미지 반환
-            completion(image)
-        }
-        
-        task.resume()
-    }
 }
