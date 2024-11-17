b5e90ddb5d6db51f7ebef37f01252be5839a877c
grgnjhyxxk <grgnjhyxxk@gmail.com>
2024-11-17 20:28:25 +0900

    layoutSubviews 설정 및 mediaContent에 쓰일 UI객체 추가

--- Geulitch/Geulitch/PublicCell/FeedTableCustomCell.swift
+++ Geulitch/Geulitch/PublicCell/FeedTableCustomCell.swift
@@ -8,41 +8,28 @@
 import UIKit
 import SnapKit
 import Then
+import AVKit
 
 class FeedTableCustomCell: UITableViewCell {
     static let identifier = "FeedTableCustomCell"
     
     private var postTitleHeightConstraint: Constraint?
     private var postBlurbHeightConstraint: Constraint?
-    
+
     let userProfileImageButton = UIButton().then {
-        $0.setImage(UIImage(named: "DefaultUserProfileImage"), for: .normal)
         $0.backgroundColor = UIColor.skeleton
         $0.layer.cornerRadius = 18
+        $0.layer.borderWidth = 0.25
+        $0.layer.borderColor = UIColor.ButtonBorderColor.cgColor
     }
-    
-    let penNameButton = UIButton().then {
-        $0.setTitle("김상민그는감히전설이라고할수있다", for: .normal)
-        $0.setTitleColor(UIColor.primaryTextColor, for: .normal)
-        $0.titleLabel?.font = UIFont.notoSansKR(size: 14, weight: .medium)
-    }
-    
+
     let userIDButton = UIButton().then {
-        $0.setTitle("crea_tio_jewelry", for: .normal)
         $0.setTitleColor(UIColor.primaryTextColor, for: .normal)
         $0.titleLabel?.font = UIFont.notoSans(size: 14, weight: .semibold)
         $0.backgroundColor = .clear
     }
     
-    let postTitleTextLabel = UILabel().then {
-        $0.text = "게시글 제목"
-        $0.textColor = UIColor.primaryTextColor
-        $0.font = UIFont.notoSansKR(size: 16, weight: .medium)
-        $0.isUserInteractionEnabled = false
-    }
-    
     let postBlurbTextLabel = UILabel().then {
-        $0.text = "본문 내용 동적인 높이 테스트, 본문 내용 동적인 높이 테스트, 본문 내용 동적인 높이 테스트, 본문 내용 동적인 높이 테스트, 본문 내용 동적인 높이 테스트, 본문 내용 동적인 높이 테스트"
         $0.textColor = UIColor.primaryTextColor
         $0.font = UIFont.notoSansKR(size: 14, weight: .regular)
         $0.isUserInteractionEnabled = false
@@ -58,35 +45,44 @@ class FeedTableCustomCell: UITableViewCell {
     }
     
     let dateLabel = UILabel().then {
-        $0.text = "1일전"
-        $0.textColor = UIColor.SubLabelTextColor.withAlphaComponent(0.3)
+        $0.textColor = UIColor.SubLabelTextColor.withAlphaComponent(0.5)
         $0.font = UIFont.notoSansKR(size: 14, weight: .regular)
     }
     
     let ellipsisButton = UIButton().then {
-        $0.setImage(UIImage(systemName: "ellipsis")?.withRenderingMode(.alwaysOriginal), for: .normal)
         $0.tintColor = UIColor.SubLabelTextColor
     }
     
-    let postLikeSatatusButton = UIButton().then {
-        $0.setTitle("좋아요·0", for: .normal)
-        $0.titleLabel?.font = UIFont.notoSansKR(size: 14, weight: .regular)
-        $0.setTitleColor(UIColor.SubButtonBackgoundColor, for: .normal)
+    let mediaContentView = UIView().then {
         $0.backgroundColor = UIColor.clear
+        $0.layer.cornerRadius = 15
+        $0.clipsToBounds = true
     }
     
-    let postCommentSatatusButton = UIButton().then {
-        $0.setTitle("댓글·0", for: .normal)
-        $0.titleLabel?.font = UIFont.notoSansKR(size: 14, weight: .regular)
-        $0.setTitleColor(UIColor.SubButtonBackgoundColor, for: .normal)
-        $0.backgroundColor = UIColor.clear
-    }
-    
-    let postPreViewImageView = UIImageView().then {
+    let mediaContentButton_1 = UIButton().then {
         $0.backgroundColor = UIColor.skeleton
-        $0.layer.cornerRadius = 10
+        $0.clipsToBounds = true
+        $0.imageView?.contentMode = .scaleAspectFill
     }
     
+    let mediaContentButton_2 = UIButton().then {
+        $0.backgroundColor = UIColor.skeleton
+        $0.clipsToBounds = true
+        $0.imageView?.contentMode = .scaleAspectFill
+    }
+
+    let mediaContentButton_3 = UIButton().then {
+        $0.backgroundColor = UIColor.skeleton
+        $0.clipsToBounds = true
+        $0.imageView?.contentMode = .scaleAspectFill
+    }
+
+    let mediaContentButton_4 = UIButton().then {
+        $0.backgroundColor = UIColor.skeleton
+        $0.clipsToBounds = true
+        $0.imageView?.contentMode = .scaleAspectFill
+    }
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         backgroundColor = .clear
@@ -101,15 +97,15 @@ class FeedTableCustomCell: UITableViewCell {
 
     private func setupView() {
         contentView.addSubview(userProfileImageButton)
-        contentView.addSubview(penNameButton)
         contentView.addSubview(userIDButton)
         contentView.addSubview(dateLabel)
         contentView.addSubview(ellipsisButton)
-        contentView.addSubview(postTitleTextLabel)
         contentView.addSubview(postBlurbTextLabel)
-        contentView.addSubview(postPreViewImageView)
-        contentView.addSubview(postLikeSatatusButton)
-        contentView.addSubview(postCommentSatatusButton)
+        contentView.addSubview(mediaContentView)
+        mediaContentView.addSubview(mediaContentButton_1)
+        mediaContentView.addSubview(mediaContentButton_2)
+        mediaContentView.addSubview(mediaContentButton_3)
+        mediaContentView.addSubview(mediaContentButton_4)
     }
     
     private func setupConstraints() {
@@ -140,12 +136,55 @@ class FeedTableCustomCell: UITableViewCell {
             make.top.equalTo(userIDButton.snp.bottom).offset(3.5)
             make.leading.equalTo(userProfileImageButton.snp.trailing).offset(10)
             make.trailing.equalTo(-15)
+            make.bottom.equalTo(mediaContentView.snp.top).offset(-10)
+        }
+        
+        mediaContentView.snp.makeConstraints { make in
+            make.height.equalTo(160)
+            make.leading.equalTo(userProfileImageButton.snp.trailing).offset(10)
+            make.trailing.equalTo(-15)
             make.bottom.equalToSuperview().offset(-15)
+        }
+        
+        mediaContentButton_1.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.5).offset(-2)
+            make.width.equalToSuperview().multipliedBy(0.5).offset(-2)
+        }
+
+        mediaContentButton_2.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.5).offset(-2)
+            make.width.equalToSuperview().multipliedBy(0.5).offset(-2)
+        }
+        
+        mediaContentButton_3.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.5).offset(-2)
+            make.width.equalToSuperview().multipliedBy(0.5).offset(-2)
+        }
+        
+        mediaContentButton_4.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.5).offset(-2)
+            make.width.equalToSuperview().multipliedBy(0.5).offset(-2)
         }
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
+        let color = UIColor.ButtonBorderColor
+        let resolvedColor = color.resolvedColor(with: traitCollection)
+        userProfileImageButton.layer.borderColor = resolvedColor.cgColor
+        userProfileImageButton.layer.borderWidth = 0.25
+        
+        mediaContentView.layer.borderColor = resolvedColor.cgColor
+        mediaContentView.layer.borderWidth = 0.25
     }
     
     private func addTargets() {
